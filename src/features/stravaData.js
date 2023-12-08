c2d46b5aae3f5f156ff65c52398a06d1f0652f1b
akshayyelvankar <akshayyelvankar@gmail.com>
2023-12-08 16:59:40 +0530

    Initial Commit

--- src/features/stravaData.js
+++ src/features/stravaData.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import moment from "moment";
-import { data_raw } from "./data/stravaData_raw";
+
 
 // export const getStravaData = createAsyncThunk(
 //   "data/getData",
